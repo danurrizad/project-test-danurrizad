c92a8b46e8a10f127193bf394e29141fd3faccfe
danurrizad <danurrizad242@gmail.com>
2023-12-23 16:00:00 +0700

    add: published date

--- src/pages/IdeasPosts.jsx
+++ src/pages/IdeasPosts.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const IdeasPosts = ({title, content}) => {
+const IdeasPosts = ({title, publishedDate, content}) => {
   return (
     <div className='min-h-screen px-20 py-40 flex flex-col justify-center items-center '>
-        <h1 className='font-bold text-[23px] text-center pb-10'>{title}</h1>
+        <h1 className='font-bold text-[32px] text-center'>{title}</h1>
+        <h2 className='pb-10 pt-2'>{publishedDate}</h2>
         <div dangerouslySetInnerHTML={{ __html: content }} />
     </div>
   )
